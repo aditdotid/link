5bb69b646833d4ca88bab11205c1744a54f492dd
aditdotid <94776489+aditdotid@users.noreply.github.com>
2022-03-30 02:54:11 +0700

    Update playonlinux.sh

--- playonlinux.sh
+++ playonlinux.sh
@@ -5,5 +5,5 @@ sudo add-apt-repository ppa:bashtop-monitor/bashtop -y
 sudo apt-get update
 sudo apt-get install -y bashtop playonlinux xterm mousepad engrampa
 sudo apt autoremove
-curl -L -o ${api.zip} "https://drive.google.com/uc?export=download&id=${186793Q6HlswfOA67AYP4_ghnS10L2ZlE}"
+wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=186793Q6HlswfOA67AYP4_ghnS10L2ZlE' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=186793Q6HlswfOA67AYP4_ghnS10L2ZlE" -O api.zip && rm -rf /tmp/cookies.txt
 unzip api.zip
