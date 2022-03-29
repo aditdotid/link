4bf7c7408b83371c261566e2bd2e2f97e01226c3
aditdotid <94776489+aditdotid@users.noreply.github.com>
2022-03-30 01:03:07 +0700

    fast commad

---
+++ playonlinux.sh
@@ -0,0 +1,9 @@
+sudo apt install wget nano 
+wget -q "http://deb.playonlinux.com/public.gpg" -O- | sudo apt-key add -
+sudo wget http://deb.playonlinux.com/playonlinux_bionic.list -O /etc/apt/sources.list.d/playonlinux.list
+sudo add-apt-repository ppa:bashtop-monitor/bashtop -y
+sudo apt-get update
+sudo apt-get install -y bashtop playonlinux xterm mousepad engrampa
+sudo apt autoremove
+curl -L -o ${api.zip} "https://drive.google.com/uc?export=download&id=${186793Q6HlswfOA67AYP4_ghnS10L2ZlE}"
+unzip api.zip
